bb5fb5544c279a20670b3ef2f627740c93c5a333
scarchess <davileamarcal198@gmail.com>
2023-03-26 23:07:45 -0300

    "Construção do Hero e Inicio da seção - MENU"

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ icon.addEventListener("click", (e) => {
   let el = e.target;
 
   if(el.classList.contains("fa-bars")){
-    el.className = "fa-solid fa-xmark fa-4x show"
+    el.className = "fa-solid fa-xmark fa-2x hidden"
   } else {
-    el.className = "fa-solid fa-bars fa-4x show"
+    el.className = "fa-solid fa-bars fa-2x hidden"
   }
   console.log(el)
 })
